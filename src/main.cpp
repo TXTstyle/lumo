263e6e1da56bf6abe41715f704076b949707022a
Oskar <oskar.fv57@gmail.com>
2024-12-18 20:37:10 +0100

    Basic Spheres

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,11 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+#include <glm/ext/matrix_float4x4.hpp>
 #include <glm/ext/matrix_transform.hpp>
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <cstdlib>
+#include <glm/trigonometric.hpp>
 #include <iostream>
 #include <array>
 
@@ -15,11 +17,18 @@
 #include "VertexBuffer.hpp"
 #include "VertexBufferLayout.hpp"
 #include "Texture.hpp"
+#include "ShaderStorage.hpp"
+
+struct Sphere {
+    glm::vec4 origin;
+    glm::vec4 color;
+};
 
 int main() {
     Vision::Renderer renderer;
+    glm::uvec2 imgSize = {1280, 720};
     try {
-        renderer.Init({1280, 720}, "dev");
+        renderer.Init(imgSize, "dev");
     } catch (const std::exception& e) {
         std::cerr << e.what() << std::endl;
         renderer.Shutdown();
@@ -30,8 +39,7 @@ int main() {
     shader.Use();
     shader.SetInt("uTex", 0);
 
-    Vision::Camera cam(renderer, {0.0f, 100.0f, 0.0f}, {0.1f, 200.0f}, 45.0f,
-                       5.0f);
+    Vision::Camera cam({0.0f, 0.0f, 0.0f}, 45.0f, 5.0f);
 
     std::array<float, 20> verts = {
         -1.0f, 1.0f,  0.0f, 0.0f, 1.0f, //
@@ -49,7 +57,35 @@ int main() {
     Vision::VertexArray va;
     va.AddBuffer(buffer, layout);
 
-    glm::uvec2 imgSize = {512, 512};
+    std::array<Sphere, 3> objects = {
+        Sphere{
+            {1.0f, 2.0f, 1.0f, 0.5f},
+            {0.0f, 1.0f, 0.0f, 1.0f},
+        },
+        Sphere{
+            {-1.0f, 1.0f, 1.0f, 1.0f},
+            {0.0f, 0.0f, 1.0f, 1.0f},
+        },
+        Sphere{
+            {0.0f, -2.0f, 0.0f, 2.0f},
+            {1.0f, 0.0f, 0.0f, 1.0f},
+        },
+    };
+    // std::array<float, 21> objects = {
+    //         1.0f, 2.0f, 1.0f,
+    //     0.5f,
+    //         0.0f, 1.0f, 0.0f,
+    //         -1.0f, 1.0f, 1.0f,
+    //         0.0f, 0.0f, 1.0f,
+    //         2.0f,
+    //         0.0f, -2.0f, 0.0f,
+    //         1.0f, 0.0f, 0.0f,
+    //         1.0f,
+    // };
+
+    Vision::ShaderStorage ssbo(objects.data(), objects.size() * sizeof(Sphere),
+                               3);
+    ssbo.Bind();
 
     Vision::ComputeShader computeShader("res/shaders/Basic.comp");
     Vision::Texture texture(imgSize.x, imgSize.y, GL_RGBA32F);
@@ -66,19 +102,23 @@ int main() {
         deltaTime = currentFrame - lastFrame;
         lastFrame = currentFrame;
         if (fCounter > 500) {
-            std::cout << "FPS: " << 1 / deltaTime << std::endl;
+            std::cout << "FPS: " << 1 / deltaTime
+                      << " CamPos: " << cam.getPos().x << " " << cam.getPos().y
+                      << " " << cam.getPos().z << std::endl;
             fCounter = 0;
         } else {
             fCounter++;
         }
-
         cam.Controls(renderer);
 
         computeShader.Use();
-        computeShader.SetFloat("uTime", currentFrame);
-        glDispatchCompute((unsigned int)imgSize.x, (unsigned int)imgSize.y, 1);
-        // make sure to writing to image has finished before read
-        glMemoryBarrier(GL_SHADER_IMAGE_ACCESS_BARRIER_BIT);
+        // computeShader.SetFloat("uTime", currentFrame);
+        computeShader.SetMat3f("uCamMat", cam.getMat());
+        computeShader.SetFloat("uFOV", cam.getFov());
+        computeShader.SetVec3f("uCamPos", cam.getPos());
+        computeShader.SetVec2i("uRes", imgSize);
+
+        computeShader.Dispatch({imgSize.x, imgSize.y, 1});
 
         renderer.Clear({0.0, 0.0, 0.0});
         shader.Use();
