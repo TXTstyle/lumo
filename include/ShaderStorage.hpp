d0cb7891b12434a80a91d77ea80162b3c36d2d53
Oskar <oskar.fv57@gmail.com>
2024-12-18 20:36:44 +0100

    ShaderStorage fix

--- include/ShaderStorage.hpp
+++ include/ShaderStorage.hpp
@@ -7,7 +7,7 @@ class ShaderStorage {
     uint32_t id;
 
   public:
-    ShaderStorage(const void* data, uint32_t size);
+    ShaderStorage(const void* data, uint32_t size, const uint32_t bindIdx);
     ~ShaderStorage();
 
     void SubData(uint32_t offset, uint32_t size, const void* data);
